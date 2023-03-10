a756e419b6aec9ecdcf7ef40c8b35027acab2a55
Matteo Guarrera <34736894+matteoguarrera@users.noreply.github.com>
2023-03-10 15:49:41 -0800

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -6,7 +6,7 @@ class RecurrentClassifier(nn.Module):
     super().__init__()
     
     self.output_size = 1
-    dropout = 0.3
+    dropout = 0.0
     if model_type == 'rnn':
       self.model = nn.RNN(input_size = 1, 
                           hidden_size = hidden_size,
