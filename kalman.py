c4756a13cfdbf47b7341e98b739c66701921eca2
carlobosio <60157010+carlobosio@users.noreply.github.com>
2023-03-10 17:28:51 -0800

    Update kalman.py
    x -> h, z -> x

--- kalman.py
+++ kalman.py
@@ -8,7 +8,7 @@ class KalmanFilter(object):
                  Q, # covariance of process noise [constant, assume iid]
                  R, # covariance measurement noise [constant, assume iid]
                  P, # 
-                 x0):
+                 h0):
 
         self.F = F
         self.dF = dF
@@ -16,23 +16,23 @@ class KalmanFilter(object):
         self.Q = Q 
         self.R = R 
         self.P = P 
-        self.x = x0 
+        self.h = h0 
     
     def predict(self, u = 0):
-        self.x = self.F(self.x) # + np.dot(self.B, u)
+        self.h = self.F(self.h) # + np.dot(self.B, u)
 
-        self.P = self.dF(self.x) * self.P * self.dF(self.x) + self.Q
+        self.P = self.dF(self.h) * self.P * self.dF(self.h) + self.Q
 
-    def update(self, z): # z: measurement
-        y = z - self.H * self.x # helper
+    def update(self, x): # x: measurement
+        y = x - self.H * self.h # helper
 
         S = self.R + self.H * self.P * self.H # helper for Kalman gain
 
-        K = self.P * self.H /S # Kalman gain
-        self.x = self.x + K * y # y is the disagrement 
+        K = self.P * self.H / S # Kalman gain
+        self.h = self.h + K * y # y is the disagrement 
 
         # I = np.eye(self.n)
         _quad_ = (1 - K * self.H)  
         self.P = _quad_ * self.P * _quad_ + K * self.R * K
 
-        return self.x
+        return self.h
