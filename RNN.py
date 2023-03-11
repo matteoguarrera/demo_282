bba4fb23a831a2c686d84f5deda63d4aaae0fb2b
Matteo Guarrera <34736894+matteoguarrera@users.noreply.github.com>
2023-03-10 19:36:38 -0800

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -18,16 +18,12 @@ class RecurrentClassifier(nn.Module):
                           num_layers = num_layers, dropout = dropout, batch_first = True)
 
     self.linear = nn.Linear(hidden_size, self.output_size)
-    # self.drop = nn.Dropout(p=0.5)
 
   def forward(self, x: torch.Tensor, h0 = None) -> torch.Tensor:
 
     # output features (h_t) from the last layer of the RNN
     # h_n the final hidden state for each element in the batch
     output, h_n = self.model(x, h0)
-    # output = self.drop(output)
-    # output = se
     output = self.linear(output)
     
-    # print(output1.shape, output2.shape)
     return output
